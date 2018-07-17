af78eed74b0744dc771955d2d80fddff5bb82542
guestgraphics <ictman1076@sorcerertech.pcriot.com>
2018-07-17 10:15:18 +0100

    ex 2.2

--- page.js
+++ page.js
@@ -1,5 +1,8 @@
 jQuery(function($){
     $('.close-box').on('click', function (){
         $('.my-custom-box').slideUp();
-    })
+    });
+    $('.yt-modal-btn').onl('click', function (){
+        $('#video-modal').modal('show');
+    });
 });
